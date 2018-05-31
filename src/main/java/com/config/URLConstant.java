f3239b7bc19c632f0f73b274ef6a8848c462deae
bruce.lxm <bruce.lxm@alibaba-inc.com>
2018-05-31 19:35:50 +0800

    production env oapi & delete appSec

--- src/main/java/com/config/URLConstant.java
+++ src/main/java/com/config/URLConstant.java
@@ -5,6 +5,6 @@ public class URLConstant {
     /**
      *获取用户基础信息的接口URL
      */
-    public static final String URL_GET_USERINFO_BY_CODE = "https://pre-oapi.dingtalk.com/sns/getuserinfo_bycode";
+    public static final String URL_GET_USERINFO_BY_CODE = "https://oapi.dingtalk.com/sns/getuserinfo_bycode";
 
 }
